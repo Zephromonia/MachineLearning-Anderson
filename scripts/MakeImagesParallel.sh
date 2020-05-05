981707d714431bc04dd553e9a6ab4c25dec46c4a
RudoRoemer <r.roemer@warwick.ac.uk>
2020-05-05 11:49:56 +0100

    new parallel data generation script

---
+++ scripts/MakeImagesParallel.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+size=${1:-20}
+nsamples=${2:-2}
+color=${3:-1}
+frame=${4:-0}
+type=${5:-jpg}
+imgsize=${6:-0}
+
+echo $size $nsamples $type
+
+#datadir="/media/phsht/DataDrive/AML3D_data"
+datadir="/storage/disqs/phsht/Archive-DATA/MULTIFRACTALS/AML3D_data"
+#datadir="/mnt/md0/phsht/data/AML3D_data"
+
+MLdir=`pwd`
+echo $MLdir
+
+#WFPLOT=$HOME/Projects/MachineLearning/WFplot/WFplot.GF
+WFPLOT=/storage/disqs/MachineLearning-Anderson/WFplot/WFplot.GF
+#WFPLOT=$MLdir"/../WFplot/WFplot.GF"
+#WFPLOT=/media/phsht/DataDrive/MachineLearning/Anderson/WFplot/WFplot.GF
+
+# copy the original data files
+
+cd $datadir
+pwd
+
+for disdir in W*/
+do
+
+    echo $disdir
+    #cd $disdir
+
+    for dir in `ls -d $disdir/L$size/AM-* | head -$nsamples`
+    do
+	echo $dir
+	mkdir -p $MLdir/$disdir
+	cp -u --preserve=timestamps $dir/Evec*.bz2 $MLdir/$disdir
+    done
+
+    #cd ..
+done
+
+# make images from the copied data files
+
+cd $MLdir
+pwd
+
+for disdir in W*/
+do
+
+    echo $disdir
+    cd $disdir
+    #bunzip2 -f *$size*.bz2
+    find . -name "*.raw.bz2" | parallel -I% --max-args 1 bunzip2 %
+
+#    for evec in Evec*.raw
+#    do
+#	echo $evec
+#	echo -ne "$evec\n$color\n$frame" | $WFPLOT
+#	if [ $imgsize -lt 1 ]
+#	then
+#	    convert $evec.eps `basename $evec .raw.eps`.$type
+#	else
+#	    convert -resize $imgsize"x"$imgsize\! $evec.eps `basename $evec .raw.eps`.$type
+#	fi
+
+    find . -name "*.raw" | parallel -I% --max-args 1 (echo -ne "%\n$color\n$frame" | $WFPLOT)
+
+    if [ $imgsize -lt 1 ]
+    then
+	find . -name "*.raw.eps" | parallel -I% --max-args 1 convert % `basename %.raw.eps`.$type
+    else
+    	find . -name "*.raw.eps" | parallel -I% --max-args 1 -resize $imgsize"x"$imgsize\! %.eps `basename %.raw.eps`.$type
+    fi
+
+#    rm -f $evec.eps $evec
+    find . -name "*.raw*" | parallel -I% --max-args 1 rm -f %
+
+#    done
+    cd ..
+done
+    
+
